888d83bbba90e34019425f70d3a91d0a4a5cd431
Michael Stiven Saa Motta <michaelsaams20@gmail.com>
2024-12-29 03:05:14 -0500

    uso de useEfect para que se puedo modificar dinamicamente la url del llamado a mi api

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,41 +1,23 @@
 import { useEffect, useState } from "react"
+import { UserList } from "./components/userList"
 
 export const UsersApp = () => {
-    const [users, setUsers] = useState([])
 
-    const fetchUsers = async() => {
-        try{
-            const response = await fetch('https://jsonplaceholder.typicode.com/users')
-            const data = await response.json()
-            console.log(data)
-            setUsers(data)    
-        }catch(error){
-            console.error(error)
-        }   
- 
+    const [endPoint, setendPoint] = useState('users')
+
+    const handldenFetch = () => {
+        setendPoint('comments')
     }
-    
-    // para que se cree al comienzo del componente 
-    useEffect( () => {
-        fetchUsers()
-    }, [])  
-   
-//  nameUsers = users.name   
-  return (
-    <>
-    <h1>lista de usuarios</h1>
-    <ol>
-        {users.map(user => <li key={user.id}> {user.name}</li>)}
-        <br />
-    </ol>    
-    <ol> 
-        {users.map(user => <li key={ user.id}> {user.website}</li>)}
-    </ol>    
- 
-    <button></button>
 
-    </>
-  )
+    return (
+      <>
+      <h1>lista de usuarios</h1>
+     
+      <UserList endPoint={endPoint}></UserList>
+      <button onClick={handldenFetch}>oprime para llamar la info </button>
+
+      </>
+    )
 }
 
 
