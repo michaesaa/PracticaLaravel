2243f9de5f6698f450eaa9790960815291f7c5c5
Michael Stiven Saa Motta <michaelsaams20@gmail.com>
2024-12-29 12:33:33 -0500

    hook con helper asincorno con async y await pasadole la funcion de la sincronia de la data al useEffect getData

--- src/hooks/useFetchUsers.js
+++ src/hooks/useFetchUsers.js
@@ -1,23 +1,21 @@
 import { useEffect, useState } from "react"
+import { fetchData } from "../helpers/fetchData"
 
 export const useFetchUsers = (endPoint) => {
 
     const [data, setData] = useState([])
     const [isLoading, setIsLoading] = useState(true)
-    const fetchdata = async() => {
-        try{
-            const response = await fetch(`https://jsonplaceholder.typicode.com/${endPoint}`)
-            const data = await response.json()
-            console.log(data)
-            setData(data)    
-            setIsLoading(false)
-        }catch(error){
-            console.error(error)
-        }   
- 
+
+
+    const getData = async () => {
+        const {data, isLoading} = await fetchData(endPoint)
+        setData(data)
+        setIsLoading(isLoading)
     }
+
+
     useEffect(() => {
-        fetchdata()
+        getData()
     }, [endPoint])
 
 
